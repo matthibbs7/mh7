14105a0df4771bc0ed06f523728fd4c6aab30684
Matthew Hibbs <89611778+matthibbs7@users.noreply.github.com>
2023-02-20 13:52:31 -0500

    Added project images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Divider, Flex, Text } from '@chakra-ui/react';
+import { Divider, Flex, Text, Image } from '@chakra-ui/react';
 export default function Home() {
   return (
     <>
@@ -13,22 +12,36 @@ export default function Home() {
         <link href="https://fonts.googleapis.com/css2?family=Gloock&display=swap" rel="stylesheet"/>
       </Head>
       <Flex justifyContent="center">
-        <Flex mt={5} px={10} py={5} bg="#181818" width="90%" maxWidth="1024px" borderRadius="10px" border="1px solid #212121" flexDirection="column">
-          <Text color="bisque" style={{fontFamily: 'Gloock'}} fontSize="4xl" fontWeight={700}>Matt Hibbs</Text>
+        <Flex mt={5} px={[3, 4, 6, 8]} py={5} bg="#181818" width="90%" maxWidth="906px" borderRadius="2px" border="1px solid #212121" flexDirection="column">
+          <Flex align="center">
+            <Image mr={3} height="38px" width="38px" src="/images/bg.png" />
+            <Text color="bisque" style={{fontFamily: 'Gloock'}} fontSize="3xl" fontWeight={700}>Matthew Hibbs</Text>
+            
+          </Flex>
           <Flex>
-          <Flex>
-            <Text _hover={{textDecoration: 'underline', cursor: 'pointer'}} fontWeight={700} mr={1}>Portfolio</Text>
-            <Text _hover={{textDecoration: 'underline', cursor: 'pointer'}} fontWeight={700} mr={1}>Research</Text>
-            <Text _hover={{textDecoration: 'underline', cursor: 'pointer'}} fontWeight={700} mr={1}>Contact</Text>
+            <Flex>
+              <Text fontSize="11.5pt" _hover={{textDecoration: 'underline', cursor: 'pointer'}} fontWeight={700} mr={1}>Portfolio</Text>
+              <Text fontSize="11.5pt" _hover={{textDecoration: 'underline', cursor: 'pointer'}} fontWeight={700} mr={1}>Research</Text>
+              <Text fontSize="11.5pt" _hover={{textDecoration: 'underline', cursor: 'pointer'}} fontWeight={700} mr={1}>Involvement</Text>
+              <Text fontSize="11.5pt" _hover={{textDecoration: 'underline', cursor: 'pointer'}} fontWeight={700} mr={1}>Contact</Text>
+            </Flex>
+            <Flex ml="auto">
+              <Text fontSize="11.5pt" _hover={{textDecoration: 'underline', cursor: 'pointer'}} fontWeight={700} mr={1}>Resume</Text>
+              <Text fontSize="11.5pt" _hover={{textDecoration: 'underline', cursor: 'pointer'}} fontWeight={700} mr={1}>Blog</Text>
+            </Flex>
           </Flex>
-          <Flex ml="auto">
-            <Text _hover={{textDecoration: 'underline', cursor: 'pointer'}} fontWeight={700} mr={1}>Resume</Text>
-            <Text _hover={{textDecoration: 'underline', cursor: 'pointer'}} fontWeight={700} mr={1}>Blog</Text>
+          <Flex borderBottom="1px solid lightgrey" mt={1} mb={3}></Flex>
+          <Text fontSize="11pt" fontWeight={500}>I&apos;m a student at the University of Florida pursuing a Bachelor&apos;s degree in Computer Science. I am passionate about mathematical modeling, UI design, high performance computing, cryptocurrencies, and web development. I&apos;m proficient in object-oriented programming languages like C++, Python, and TypeScript. I&apos;ve worked on various collaborative projects, fascilitating frontend interfaces, API designs, and backend development. Additionally, I&apos;ve dabbled in different domains of knowledge from building query parsers, CUDA programming, and implementing various statistical methods.</Text>
+          <Text fontSize="11pt" mt={3} fontWeight={500}>My industry experience includes working as a Software Engineer at FAANG, Tech Unicorns, and F50 companies</Text>
+          <Flex justifyContent="center" mt={8} mb={8} flexWrap="wrap">
+            <Image _hover={{cursor: 'pointer', border: "1px solid lightgrey"}} border="0.5px solid lightgrey" width="auto" height="auto" maxHeight="140px" maxWidth="265px" src="/images/projects/sherlock.png" />
+            <Image _hover={{cursor: 'pointer', border: "1px solid lightgrey"}} border="0.5px solid lightgrey" width="auto" height="auto" maxHeight="140px" maxWidth="230px" src="/images/projects/tcp.png" />
+            <Image _hover={{cursor: 'pointer', border: "1px solid lightgrey"}} border="0.5px solid lightgrey" width="auto" height="auto" maxHeight="140px" maxWidth="230px" src="/images/projects/educatus.jpeg" />
+            <Image _hover={{cursor: 'pointer', border: "1px solid lightgrey"}} border="0.5px solid lightgrey" width="auto" height="auto" maxHeight="140px" maxWidth="230px" src="/images/projects/haikai.png" />
+            <Image _hover={{cursor: 'pointer', border: "1px solid lightgrey"}} border="0.5px solid lightgrey" width="auto" height="auto" maxHeight="140px" maxWidth="230px" src="/images/projects/demogorgn.png" />
+            <Image _hover={{cursor: 'pointer', border: "1px solid lightgrey"}} border="0.5px solid lightgrey" width="auto" height="auto" maxHeight="140px" maxWidth="230px" src="/images/projects/plexy.png" />
+            <Image _hover={{cursor: 'pointer', border: "1px solid lightgrey"}} border="0.5px solid lightgrey" width="auto" height="auto" maxHeight="140px" maxWidth="230px" src="/images/projects/sabu.png" />
           </Flex>
-          </Flex>
-          <Divider mt={1} mb={3} bgColor="white" />
-          <Text fontWeight={500}>I&apos;m a student at the University of Florida pursuing a Bachelor&apos;s degree in Computer Science. I am passionate about mathematical modeling, UI design, high performance computing, cryptocurrencies, and web development. I&apos;m proficient in object-oriented programming languages like C++, Python, and TypeScript. I&apos;ve worked on various collaborative projects, fascilitating frontend interfaces, API designs, and backend development. Additionally, I&apos;ve dabbled in different domains of knowledge from building query parsers, CUDA programming, and implementing various statistical methods.</Text>
-          <Text mt={3} fontWeight={500}>My industry experience includes working as a Software Engineer at FAANG, Tech Unicorns, and F50 companies</Text>
         </Flex>
       </Flex>
     </>
