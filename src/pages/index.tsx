133c2a7fa4e935695cb255bce51dd506ae8859e7
Matthew Hibbs <89611778+matthibbs7@users.noreply.github.com>
2023-03-22 22:18:50 -0400

    Cleaned up code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from 'next/head'
 import { Divider, Flex, Text, Image, UnorderedList, ListItem, useColorMode, useColorModeValue, Link } from '@chakra-ui/react';
-import { FaGithub } from 'react-icons/fa'
-import Magic from '@/components/Magic';
 import { useState } from 'react';
 
 export default function Home() {
@@ -25,8 +23,6 @@ export default function Home() {
         <Flex mb={10} px={[3, 4, 6, 8]} py={[2,2,4,4]} width=
         {["94%","90%"]} maxWidth="724px" flexDirection="column">
           <Flex ml="auto">
-            {/* <Magic colorMode={colorMode} toggleColorMode={toggleColorMode} setShades={setShades} shades={shades} /> */}
-          {/* <Text _hover={{cursor: 'pointer', bg: 'lightgrey'}} border="1px solid lightgrey" borderRadius="5px" p={2} ><FaMagic color="grey" /></Text> */}
           </Flex>
           <Flex mt={[2,4,8,
         12]} flexDirection="column" align="center" justifyContent="center">
@@ -38,18 +34,6 @@ export default function Home() {
           <Flex mb={5}  mt={7} flexDirection="column">
             <Text color={color} textAlign="center" style={{fontFamily: 'Barlow'}} fontSize="19.5px" fontWeight="600" lineHeight="28px">I&apos;m a software engineer based in New York. I read <Text fontSize="19.5px" fontWeight="600" lineHeight="28px" borderBottom="3px solid" as="span" _hover={{cursor: 'pointer'}}>36 books</Text> every year. Check out some of my <Text fontSize="19.5px" fontWeight="600" lineHeight="28px" borderBottom="3px solid" as="span" _hover={{cursor: 'pointer'}}>open source projects</Text>, which have won competitions and led to academic <Text fontSize="19.5px" fontWeight="600" lineHeight="28px" borderBottom="3px solid" as="span" _hover={{cursor: 'pointer'}}>co-authorships</Text>.</Text>
             <Text color={color} mt={5} textAlign="center" style={{fontFamily: 'Barlow'}} fontSize="19.5px" fontWeight="600" lineHeight="28px">My <Text fontSize="19.5px" fontWeight="600" lineHeight="28px" borderBottom="3px solid" as="span">proficiencies</Text> include building frontend interfaces, high performance computing, web development, and systems programming. I enjoy cooking, working out, and <Text fontSize="19.5px" fontWeight="600" lineHeight="28px" borderBottom="3px solid" as="span">poker</Text> in my free time.</Text>
-            {/* <Text mt={5} textAlign="center" color={color} style={{fontFamily: 'Barlow'}} fontWeight={700}><Link>CV</Link> | <Link href="https://github.com/matthibbs7" isExternal>GitHub</Link> | <Link href="mailto: hibbs.matthew@protonmail.com">Email</Link></Text> */}
-            {/* <Text color={color} mt={5} textAlign="center" style={{fontFamily: 'Barlow'}} fontSize="19.5px" fontWeight="600" lineHeight="28px">My proficiencies include object-oriented programming languages such as C++, Python, and TypeScript. Additionally, I&apos;ve dabbled in different domains of knowledge from building query parsers, CUDA programming, implementing web sockets, to exploring various statistical methods.</Text> */}
-            {/* <Flex flexWrap="wrap">
-              <Text fontSize="11.5pt" _hover={{textDecoration: 'underline', cursor: 'pointer'}} fontWeight={700} mr={1}>Portfolio</Text>
-              <Text fontSize="11.5pt" _hover={{textDecoration: 'underline', cursor: 'pointer'}} fontWeight={700} mr={1}>Research</Text>
-              <Text fontSize="11.5pt" _hover={{textDecoration: 'underline', cursor: 'pointer'}} fontWeight={700} mr={1}>Involvement</Text>
-              <Text fontSize="11.5pt" _hover={{textDecoration: 'underline', cursor: 'pointer'}} fontWeight={700} mr={1}>Contact</Text>
-            </Flex>
-            <Flex flexWrap="wrap" ml={["0", "auto", "auto", "auto"]}>
-              <Text fontSize="11.5pt" _hover={{textDecoration: 'underline', cursor: 'pointer'}} fontWeight={700} mr={1}>Resume</Text>
-              <Text fontSize="11.5pt" _hover={{textDecoration: 'underline', cursor: 'pointer'}} fontWeight={700} mr={1}>Blog</Text>
-            </Flex> */}
           </Flex>
           <Flex id="portfolio" flexDirection="column">
             <Flex mb={3.5}  mt={7}>
@@ -144,21 +128,6 @@ export default function Home() {
           <Flex justifyContent="center" mt={10}>
             <Text color={color} style={{fontFamily: 'Barlow'}} fontSize="16px" fontWeight={300}>© 2023 Matthew Hibbs ♡</Text>
           </Flex>
-          {/* <Text fontSize="11pt" fontWeight={500}>I&apos;m a student at the University of Florida pursuing a Bachelor&apos;s degree in Computer Science. I am passionate about mathematical modeling, UI design, high performance computing, cryptocurrencies, and web development. I&apos;m proficient in object-oriented programming languages like C++, Python, and TypeScript. I&apos;ve worked on various collaborative projects, fascilitating frontend interfaces, API designs, and backend development. Additionally, I&apos;ve dabbled in different domains of knowledge from building query parsers, CUDA programming, implementing web sockets, to exploring various statistical methods.</Text>
-          <Text fontSize="11pt" mt={3} fontWeight={500}>My industry experience includes working as a Software Engineer at FAANG, Tech Unicorns, and F50 companies</Text>
-          <Flex justifyContent="center" mt={8} mb={8} flexWrap="wrap">
-            <Image _hover={{cursor: 'pointer', border: "1px solid lightgrey"}} border="0.5px solid lightgrey" width="auto" height="auto" maxHeight="140px" maxWidth="265px" src="/images/projects/sherlock.png" />
-            <Image _hover={{cursor: 'pointer', border: "1px solid lightgrey"}} border="0.5px solid lightgrey" width="auto" height="auto" maxHeight="140px" maxWidth="230px" src="/images/projects/tcp.png" />
-            <Image _hover={{cursor: 'pointer', border: "1px solid lightgrey"}} border="0.5px solid lightgrey" width="auto" height="auto" maxHeight="140px" maxWidth="230px" src="/images/projects/educatus.jpeg" />
-            <Image _hover={{cursor: 'pointer', border: "1px solid lightgrey"}} border="0.5px solid lightgrey" width="auto" height="auto" maxHeight="140px" maxWidth="230px" src="/images/projects/haikai.png" />
-            <Image _hover={{cursor: 'pointer', border: "1px solid lightgrey"}} border="0.5px solid lightgrey" width="auto" height="auto" maxHeight="140px" maxWidth="230px" src="/images/projects/demogorgn.png" />
-            <Image _hover={{cursor: 'pointer', border: "1px solid lightgrey"}} border="0.5px solid lightgrey" width="auto" height="auto" maxHeight="140px" maxWidth="230px" src="/images/projects/plexy2.png" />
-
-            <Image _hover={{cursor: 'pointer', border: "1px solid lightgrey"}} border="0.5px solid lightgrey" width="auto" height="auto" maxHeight="140px" maxWidth="253px" src="/images/projects/gsta.png" />
-            <Image _hover={{cursor: 'pointer', border: "1px solid lightgrey"}} border="0.5px solid lightgrey" width="auto" height="auto" maxHeight="140px" maxWidth="230px" src="/images/projects/ds.png" />
-            
-            <Image _hover={{cursor: 'pointer', border: "1px solid lightgrey"}} border="0.5px solid lightgrey" width="auto" height="auto" maxHeight="140px" maxWidth="230px" src="/images/projects/sabu.png" />
-          </Flex> */}
         </Flex>
       </Flex>
     </>
